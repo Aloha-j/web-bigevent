60849113353fd8c57c7a53af865ee4f8a7130268
Aloha-j <503430078@qq.com>
2022-11-04 17:28:14 +0800

    完成了主页的开发

--- 素材/js/1.js
+++ 素材/js/1.js
@@ -1,4 +1,16 @@
 
-$.ajaxPrefilter(function(options) {
-    options.url = 'http://www.liulongbin.top:3007' + options.url
-  })
+$.ajaxPrefilter(function (options) {
+  options.url = 'http://big-event-api-t.itheima.net' + options.url
+
+  if (options.url.indexOf('/my/') !== -1) {
+    options.header = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+  options.Complete = function (res) {
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+    }
+  }
+})
---
+++ 素材/js/index.js
@@ -0,0 +1,54 @@
+$(function () {
+    getUserInfo()
+    var layer = layui.layer
+    $('#btnLogout').on('click', function () {
+        // console.log('ok');
+        layer.confirm('确定退出?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // console.log('ok');
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+
+
+function getUserInfo() {
+    $.ajax({
+        method: 'get',
+        url: '/my/userinfo',
+        // Header: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取信息失败')
+            }
+            renderAvatar(res.data)
+        },
+        // Complete: function (res) {
+        //     // console.log('执行了complete回调');
+        //     // console.log(res);
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
+        //         localStorage.removeItem('token')
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+
+}
+
+function renderAvatar(user) {
+    var name = user.nickname || user.name
+    $('welcome').html('欢迎&nbsp;欢迎&nbsp;' + name)
+}
+
+if (user.user_pic !== null) {
+    $('.layui-nav-img').attr('src', user.user_pic).show()
+    $('.text.-avatar').hide()
+} else {
+    $('.layui-nav-img').hide()
+    var first = name[0].toUpperCase()
+    $('.text-avatar').html(first).show()
+}
